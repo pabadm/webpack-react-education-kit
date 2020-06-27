411f01d848b5aae0baf43afb18e357f74a9be559
pabadm <balanpasha9@gmail.com>
2020-06-27 20:29:05 +0300

    updated config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "/build"),
-    filename: "index-bundle.js"
+    path: path.join(__dirname, '/build'),
+    filename: 'index-bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    })
+      template: './public/index.html',
+    }),
   ],
+  devServer: {
+    historyApiFallback: true,
+    compress: true,
+    port: 7070,
+    noInfo: true,
+    quiet: true,
+    clientLogLevel: 'warning',
+    stats: 'errors-only',
+    open: true,
+  },
 };
