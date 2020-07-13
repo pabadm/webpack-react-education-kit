b6dce1b66f4555805bc2b07379fb24c35d966951
pabadm <balanpasha9@gmail.com>
2020-07-13 23:26:32 +0300

    set up sanitize.css (like normalize.css)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-// key da3cb846a4228da5ed81f3869652dea8
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './index.css';
